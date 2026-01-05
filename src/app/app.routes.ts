47d795e0ce2ce4cddb5b2882cdc16b80e77a66cc
Kaan <kaan403@icloud.com>
2026-01-05 14:47:09 +0100

    feat(ui): update website branding and page title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import {Register} from './features/auth/register/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'register', component: Register },
+  { path: 'login', component: Login, title: '2Do | Welcome' },
+  { path: 'dashboard', component: Dashboard, title: '2Do | My Dashboard' },
+  { path: 'register', component: Register, title: '2Do | Register' },
   { path: '**', redirectTo: 'login' }
 ];
