cdb36a5c10687bccdb6e37a677fc14ca4853ac59
Kaan <kaan403@icloud.com>
2026-03-14 03:49:51 +0100

    feat: shared footer component, left-aligned about page with feature cards

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, Navbar],
+  imports: [RouterLink, Navbar, Footer],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
